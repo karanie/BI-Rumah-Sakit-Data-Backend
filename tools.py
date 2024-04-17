34b5f0162d4436914eda0dd8d574df591dd503f8
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2024-04-17 15:33:54 +0700

    Restrict amount of columns that used from the dataset and set their appropriatedata type

--- tools.py
+++ tools.py
@@ -13,18 +13,60 @@ def read_dataset_pickle(files, save_as_pickle=True):
                 out.append(pickle.load(f))
         # Otherwise, read the xlsx file and save it in pickle if save_as_pickle parameter is True.
         else:
-            out.append(pd.read_excel(i + ".xlsx"))
+            out.append(read_dataset(i + ".xlsx"))
             if (save_as_pickle):
                 save_dataset_as_pickle(out[len(out) - 1], i)
     return out
 
 def read_dataset(path):
     ext = path.rsplit('.', 1)[1].lower()
+
+    cols = ["id_registrasi",
+            "id_pasien",
+            "jenis_kelamin",
+            "ttl",
+            "provinsi",
+            "kabupaten",
+            "rujukan",
+            "no_registrasi",
+            "jenis_registrasi",
+            "fix_pasien_baru",
+            "nama_departemen",
+            "jenis_penjamin",
+            "diagnosa_primer",
+            "nama_instansi_utama",
+            "waktu_registrasi",
+            "total_semua_hpp",
+            "total_tagihan",
+            "tanggal_lahir",
+            "tglPulang",
+            "usia",
+            "kategori_usia"
+    ]
+    dtype = {
+            "jenis_kelamin": "category",
+            "provinsi": "category",
+            "kabupaten": "category",
+            "rujukan": "category",
+            "jenis_registrasi": "category",
+            "fix_pasien_baru": "category",
+            "nama_departemen": "category",
+            "jenis_penjamin": "category",
+            "diagnosa_primer": "category",
+            "nama_instansi_utama": "category",
+            "kategori_usia": "category",
+    }
+    parse_dates = [
+        "waktu_registrasi",
+        "tanggal_lahir",
+        "tglPulang"
+    ]
+
     read_map = {
             "csv": pd.read_csv,
             "xlsx": pd.read_excel,
             }
-    return read_map[ext](path)
+    return read_map[ext](path, usecols=cols, dtype=dtype, parse_dates=parse_dates)
 
 def save_dataset_as_pickle(df, filename):
     with pgzip.open(filename + ".pkl.gz", "wb", thread=0) as f:
