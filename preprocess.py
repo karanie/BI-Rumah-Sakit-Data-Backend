0ab9e8f0553cd6f639ff1329691189dcf286a379
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2024-04-17 15:35:09 +0700

    Adapt preprocess to data type changes made in previous commit

--- preprocess.py
+++ preprocess.py
@@ -2,26 +2,37 @@ import pandas as pd
 import numpy as np
 
 def convert_kabupaten_na(df):
+    df["kabupaten"] = df["kabupaten"].astype(str)
     df["kabupaten"] = df["kabupaten"].fillna("Tidak diketahui")
+    df["kabupaten"] = df["kabupaten"].astype("category")
     return df
 
 def convert_kabupaten_name(df):
+    df["kabupaten"] = df["kabupaten"].astype(str)
+
     df.loc[df["kabupaten"] == "KABUPATEN S I A K", "kabupaten"] = "KABUPATEN SIAK"
     df.loc[df["kabupaten"] == "KOTA B A T A M", "kabupaten"] = "KABUPATEN BATAM"
     df.loc[df["kabupaten"] == "KOTA D U M A I", "kabupaten"] = "KOTA DUMAI"
     df["kabupaten"] = df["kabupaten"].replace("^KABUPATEN", "KAB.", regex=True)
+
+    df["kabupaten"] = df["kabupaten"].astype("category")
+    df.loc[df["kabupaten"] == "Nan", "kabupaten"] = np.NaN
+    df.loc[:, "kabupaten"] = df["kabupaten"].cat.remove_unused_categories()
     return df
 
 def convert_kabupaten_casing(df):
+    df["kabupaten"] = df["kabupaten"].astype(str)
+
     df["kabupaten"] = df["kabupaten"].str.title()
+
+    df["kabupaten"] = df["kabupaten"].astype("category")
+    df.loc[df["kabupaten"] == "Nan", "kabupaten"] = np.NaN
+    df.loc[:, "kabupaten"] = df["kabupaten"].cat.remove_unused_categories()
     return df
 
 def drop_gender_ambigu(df):
     df.loc[df["jenis_kelamin"] == "Ambigu", "jenis_kelamin"] = np.NaN
-    return df
-
-def convert_waktu_registrasi_to_datetime(df):
-    df["waktu_registrasi"] = pd.to_datetime(df["waktu_registrasi"])
+    df["jenis_kelamin"] = df["jenis_kelamin"].cat.remove_unused_categories()
     return df
 
 def drop_duplicates(df):
@@ -35,6 +46,7 @@ def convert_rujukan(df):
 def convert_gender_name(df):
     df.loc[df["jenis_kelamin"] == "perempuan", "jenis_kelamin"] = "Perempuan"
     df.loc[df["jenis_kelamin"] == "laki-laki", "jenis_kelamin"] = "Laki-laki"
+    dc1["jenis_kelamin"] = dc1["jenis_kelamin"].cat.remove_unused_categories()
     return df
 
 def sort_date_values(df):
@@ -47,7 +59,6 @@ def preprocess_dataset(df):
             convert_kabupaten_name,
             convert_kabupaten_casing,
             drop_gender_ambigu,
-            convert_waktu_registrasi_to_datetime,
             drop_duplicates,
             convert_rujukan,
             convert_gender_name,
