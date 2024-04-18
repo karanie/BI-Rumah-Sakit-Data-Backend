bd77520008295775c9f23df287fc46cec4a6afb5
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2024-04-18 10:57:09 +0700

    Open the latest modified pickle/excel file

--- tools.py
+++ tools.py
@@ -6,14 +6,23 @@ import pgzip
 def read_dataset_pickle(files, save_as_pickle=True):
     out = []
     for i in files:
+        pickle_file = i + ".pkl.gz"
+        excel_file = i + ".xlsx"
+
+        pickle_exists = os.path.isfile(pickle_file)
+        excel_exists = os.path.isfile(excel_file)
+
+        pickle_mtime = os.path.getmtime(pickle_file) if pickle_exists else 0
+        excel_mtime = os.path.getmtime(excel_file) if excel_exists else 0
+
         # Read the pickle file of the pandas dataframe if it exists.
         # This prevents a really long time file reading by pandas.
-        if os.path.isfile(i + ".pkl.gz"):
-            with pgzip.open(i + ".pkl.gz", "rb", thread=0) as f:
+        if pickle_exists and (not excel_exists or pickle_mtime >= excel_mtime):
+            with pgzip.open(pickle_file, "rb", thread=0) as f:
                 out.append(pickle.load(f))
         # Otherwise, read the xlsx file and save it in pickle if save_as_pickle parameter is True.
         else:
-            out.append(read_dataset(i + ".xlsx"))
+            out.append(read_dataset(excel_file))
             if (save_as_pickle):
                 save_dataset_as_pickle(out[len(out) - 1], i)
     return out
