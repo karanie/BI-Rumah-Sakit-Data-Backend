4aa8f345c4124b723e0a9aae46eb756616581919
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2024-05-14 09:48:01 +0700

    Add `generateRoutes.py` file

---
+++ generateRoutes.py
@@ -0,0 +1,48 @@
+from flask import request
+from filterdf import filtertime
+from getdata import getTimeSeriesData, getExponentialSmoothingForecastData
+
+def generate_route_callback(name, df, timeCol, categoricalCols=[], numericalCols=[]):
+    def callback():
+        tipe_data = request.args.get("tipe_data")
+        timeseries = request.args.get("timeseries", type=bool)
+        forecast = request.args.get("forecast", type=bool)
+        tahun = request.args.get("tahun", type=int)
+        bulan = request.args.get("bulan", type=int)
+        relative_time = request.args.get("relative_time", type=str)
+        start_date = request.args.get("start_date", type=str)
+        end_date = request.args.get("end_date", type=str)
+        resample = request.args.get("resample", type=str)
+
+        temp_df = df[[timeCol, *categoricalCols, *numericalCols, "provinsi"]]
+        temp_df = temp_df.loc[temp_df["provinsi"] == "RIAU"]
+
+        temp_df = filtertime(temp_df, timeCol, month=bulan, year=tahun, relative_time=relative_time, start_date=start_date, end_date=end_date)
+
+        data = {}
+        if not timeseries and not forecast:
+            # TODO: handle multiple columns if needed. This code currently only
+            # returns the first columns of the given categoricalCols. The
+            # numericalCols is ignored
+            if not categoricalCols:
+                raise Exception("Only one categoricalCols is supported")
+            data["index"] = temp_df[categoricalCols[0]].value_counts().index.values.tolist()
+            data["values"] = temp_df[categoricalCols[0]].value_counts().values.tolist()
+            return data
+
+        if timeseries:
+            return getTimeSeriesData(temp_df, timeCol, resample=resample, categoricalCols=categoricalCols, numericalCols=numericalCols)
+
+        if forecast:
+            return getExponentialSmoothingForecastData(temp_df, timeCol, resample=resample, categoricalCols=categoricalCols, numericalCols=numericalCols)
+
+        return data
+
+    callback.__name__ = name
+    return callback
+
+def init_routes_data(app, routes):
+    for r in routes:
+        app.route(f"/api/data{r['route']}", methods=["GET"])(r['callback'])
+    print("Data routes initialized")
+    return app
