2636a0e22acae2af3998842192a90b8af542a968
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2024-05-24 10:23:01 +0700

    Refactor the getdata functions to into three type of data: aggregate data, timeseries data, and forecast data. Generated route now have these three query options. If none of these three query is requested, it will return the original data from the dataset---similiar to timeseries except it doesn't get resampled.

--- getdata.py
+++ getdata.py
@@ -1,10 +1,18 @@
 import os
 import pickle
+import numpy as np
 import pandas as pd
 from darts.timeseries import TimeSeries
 from darts.models import ExponentialSmoothing
 
-def get_data(df, timeCol, resample="D", categoricalCols=[], numericalCols=[], pivot=False):
+def get_aggr_func(aggr_str):
+    map = {
+        "sum": np.sum,
+        "mean": np.mean,
+    }
+    return map[aggr_str]
+
+def get_time_series_df(df, timeCol=None, resample="D", categoricalCols=[], numericalCols=[], pivot=False, agg="sum"):
     if not categoricalCols and not numericalCols:
         raise Exception("Either categoricalCols or numericalCols is required")
 
@@ -15,20 +23,43 @@ def get_data(df, timeCol, resample="D", categoricalCols=[], numericalCols=[], pi
         temp_df_num = temp_df[[*numericalCols]]
         temp_df = temp_df_cat.merge(temp_df_num, left_index=True, right_index=True)
     elif categoricalCols and numericalCols and pivot:
-        temp_df = temp_df.pivot_table(index=[timeCol], columns=categoricalCols, values=(numericalCols[0] if len(numericalCols) > 1 else numericalCols[0]), fill_value=0)
+        temp_df = temp_df.pivot_table(index=[timeCol], columns=categoricalCols, values=(numericalCols if len(numericalCols) > 1 else numericalCols[0]), fill_value=0)
     elif categoricalCols and not numericalCols:
         temp_df = pd.crosstab(temp_df.index, [ temp_df[i] for i in categoricalCols ])
     else:
         temp_df_num = temp_df[[*numericalCols]]
         temp_df = temp_df_num
 
-    temp_df = temp_df.resample(resample).sum()
+    temp_df = temp_df.resample(resample).agg(get_aggr_func(agg))
     return temp_df
 
-def get_time_series_data(df, timeCol, resample="D", categoricalCols=[], numericalCols=[], timef="%Y-%m-%d", pivot=False):
+def get_aggregate_data(df, resample="", categoricalCols=[], numericalCols=[], pivot=False, agg="sum"):
+    if not categoricalCols and not numericalCols:
+        raise Exception("Either categoricalCols or numericalCols is required")
+
     data = {}
 
-    temp_df = get_data(df, timeCol, resample=resample, categoricalCols=categoricalCols, numericalCols=numericalCols, pivot=pivot)
+    temp_df = df[[*categoricalCols, *numericalCols]]
+    if categoricalCols and numericalCols and not pivot:
+        temp_df_cat = temp_df[[*categoricalCols]].groupby([temp_df.index] + categoricalCols).size().unstack(fill_value=0).reset_index()
+        temp_df_num = temp_df[[*numericalCols]].reset_index()
+        temp_df = temp_df_cat.merge(temp_df_num, left_index=True, right_index=True).agg(get_aggr_func(agg))
+    elif categoricalCols and numericalCols and pivot:
+        temp_df = temp_df.pivot_table(columns=categoricalCols, values=(numericalCols if len(numericalCols) > 1 else numericalCols[0]), fill_value=0).agg(get_aggr_func(agg))
+    elif categoricalCols and not numericalCols:
+        temp_df = temp_df[[*categoricalCols]].groupby([temp_df.index] + categoricalCols).size().unstack(fill_value=0).agg(get_aggr_func(agg))
+    else:
+        temp_df = temp_df.agg(get_aggr_func(agg))
+
+    data["index"] = temp_df.index.tolist()
+    data["values"] = temp_df.fillna(0).values.transpose().tolist()
+
+    return data
+
+def get_time_series_data(df, timeCol, resample="D", categoricalCols=[], numericalCols=[], timef="%Y-%m-%d", pivot=False, agg="sum"):
+    data = {}
+
+    temp_df = get_time_series_df(df, timeCol, resample=resample, categoricalCols=categoricalCols, numericalCols=numericalCols, pivot=pivot, agg=agg)
 
     data["index"] = temp_df.index.strftime(timef).tolist()
     data["columns"] = temp_df.columns.tolist()
@@ -36,10 +67,10 @@ def get_time_series_data(df, timeCol, resample="D", categoricalCols=[], numerica
 
     return data
 
-def get_exponential_smoothing_forecast_data(df, timeCol, resample="D", categoricalCols=[], numericalCols=[], timef="%Y-%m-%d", pivot=False):
+def get_exponential_smoothing_forecast_data(df, timeCol, resample="D", categoricalCols=[], numericalCols=[], timef="%Y-%m-%d", pivot=False, agg="sum"):
     data = {}
 
-    temp_df = get_data(df, timeCol, resample=resample, categoricalCols=categoricalCols, numericalCols=numericalCols, pivot=pivot)
+    temp_df = get_time_series_df(df, timeCol, resample=resample, categoricalCols=categoricalCols, numericalCols=numericalCols, pivot=pivot, agg=agg)
 
     data = []
     for i in temp_df.columns:
@@ -58,10 +89,10 @@ def get_exponential_smoothing_forecast_data(df, timeCol, resample="D", categoric
 
     return data
 
-def get_prophet_forecast_data(df, timeCol, models, resample="D", categoricalCols=[], numericalCols=[], timef="%Y-%m-%d", pivot=False):
+def get_prophet_forecast_data(df, timeCol, models, resample="D", categoricalCols=[], numericalCols=[], timef="%Y-%m-%d", pivot=False, agg="sum"):
     data = {}
 
-    temp_df = get_data(df, timeCol, resample=resample, categoricalCols=categoricalCols, numericalCols=numericalCols, pivot=pivot)
+    temp_df = get_time_series_df(df, timeCol, resample=resample, categoricalCols=categoricalCols, numericalCols=numericalCols, pivot=pivot, agg=agg)
 
     loaded_models = []
     for i in models:
