bbc6aeab773408d1332aeb7fe87afb0768532a8c
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2025-02-16 19:37:52 +0700

    Set configs from environment variables

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
-ALLOWED_EXTENSIONS = { "csv", "xlsx" }
-DATASET_PATH = "data/dataset/DC1"
-UPLOAD_FOLDER = "data/dataset/uploaded/"
+import os
+
+ALLOWED_EXTENSIONS = os.environ.get("ALLOWED_EXTENSIONS", "csv;xlsx").split(";")
+DATASET_PATH = os.environ.get("DATASET_PATH", "data/dataset/DC1")
+UPLOAD_FOLDER = os.environ.get("UPLOAD_FOLDER", "data/dataset/uploaded/")
+DB_TABLE = os.environ.get("DB_TABLE", "dataset")
+DB_CONNECTION = os.environ.get("DB_CONNECTION", ":memory:")
