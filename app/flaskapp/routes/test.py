0d327a8e92cdcdcd506bbc2b129a80984e85cba6
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2025-05-16 22:11:47 +0700

    Set test seeddb route to use `seed_df_to_db()` function

--- app/flaskapp/routes/test.py
+++ app/flaskapp/routes/test.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 from ..data import dataset as d
 import config
-from datastore.rdbms import upsert_df
+from datastore.rdbms import seed_df_to_db
 
 routes_test = Blueprint("routes_test", __name__)
 @routes_test.route("/api/test/seeddb", methods=["POST"])
 def seed_ma_db_in_wet_rice_field():
-    upsert_df(d.dataset, config.DB_TABLE, config.DB_CONNECTION)
+    seed_df_to_db(d, config.DB_TABLE, config.DB_CONNECTION)
     return "success"
