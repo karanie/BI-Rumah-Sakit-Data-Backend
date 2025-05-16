19bb8c379907af67f7fb5fb5d16afb537070e770
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2025-05-16 22:53:05 +0700

    Initialize datastore-dbms, run dummyapi, run flask app and run fastapi app [all of these are only for development purposes]

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-uv run python main.py app flask
+uv run python main.py init datastore-dbms
+
+uv run python -m uvicorn dummyapi:app --host 0.0.0.0 &
+uv run python main.py app flask &
+uv run python main.py app fastapi
