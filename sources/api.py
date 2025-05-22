eaa5daa9f6c8fa14e7215832c8447c3bdcfc1834
Naatie <muhammadqarani@gmail.com>
2025-05-22 17:34:45 +0700

    Add `SourceAPI` class

--- sources/api.py
+++ sources/api.py
@@ -1,2 +1,15 @@
+import requests
+import json
+import polars as pl
 
-# TODO
+class SourceAPI():
+    def __init__(self, backend = "polars"):
+        self._backend = backend
+
+    def fetch(self, url, params=None):
+        res = requests.get(url, params=params)
+        if self._backend == "object":
+            return json.loads(res.content.decode())
+        if self._backend == "polars":
+            return pl.read_json(res.content)
+
