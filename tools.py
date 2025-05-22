f1179ddc6b3ded9d0e1ca8476fad0b7562671a1b
Naatie <muhammadqarani@gmail.com>
2025-05-22 17:32:24 +0700

    Add `get_last_waktu_regis()` function in `tools.py`

--- tools.py
+++ tools.py
@@ -1,7 +1,20 @@
 import time
+import config
 
 def measure_time(func):
     time_start = time.time()
     res = func()
     time_end = time.time()
     return time_end - time_start, res
+
+def get_last_waktu_regis():
+    import sqlalchemy
+    # Get latest waktu_registrasi in table
+    engine = sqlalchemy.create_engine(config.DB_CONNECTION)
+    with engine.connect() as conn:
+        try:
+            res_current_time = conn.execute(sqlalchemy.text("SELECT waktu_registrasi FROM dataset ORDER BY Waktu_registrasi DESC LIMIT 1;")).first()
+            current_date = res_current_time[0].strftime("%Y-%m-%d %H:%M:%S")
+        except Exception as e:
+            return None
+    return current_date
