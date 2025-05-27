a5d5b6dd60f0679d766bf9704296099468118d3b
Naatie <muhammadqarani@gmail.com>
2025-05-27 18:20:56 +0700

    Provide global `DatastoreDB` `ds` in tools.py

--- tools.py
+++ tools.py
@@ -1,5 +1,9 @@
 import time
 import config
+from datastore.rdbms import DatastoreDB
+import sqlalchemy
+
+ds = DatastoreDB(backend="polars")
 
 def measure_time(func):
     time_start = time.time()
@@ -8,13 +12,9 @@ def measure_time(func):
     return time_end - time_start, res
 
 def get_last_waktu_regis():
-    import sqlalchemy
-    # Get latest waktu_registrasi in table
-    engine = sqlalchemy.create_engine(config.DB_CONNECTION)
-    with engine.connect() as conn:
-        try:
-            res_current_time = conn.execute(sqlalchemy.text("SELECT waktu_registrasi FROM dataset ORDER BY Waktu_registrasi DESC LIMIT 1;")).first()
-            current_date = res_current_time[0].strftime("%Y-%m-%d %H:%M:%S")
-        except Exception as e:
-            return None
-    return current_date
+    try:
+        res_current_time = ds.execute(sqlalchemy.text("SELECT waktu_registrasi FROM dataset ORDER BY Waktu_registrasi DESC LIMIT 1;")).first()
+    except Exception as e:
+        return None
+    current_datetime = res_current_time[0].strftime("%Y-%m-%d %H:%M:%S")
+    return current_datetime
