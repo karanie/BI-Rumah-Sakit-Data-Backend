f4073c26ea712712b1e213ab522ff51090d37a74
Naatie <muhammadqarani@gmail.com>
2025-06-03 15:45:12 +0700

    Run only the api-pollers and fastapi app

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-uv run python main.py init datastore-dbms
-
-uv run python -m uvicorn dummyapi:app --host 0.0.0.0 &
 uv run python -u main.py worker api-pollers &
-uv run python main.py app flask &
+
+# HTTP API. Flask is deprecated. Please use FastAPI
+# uv run python main.py app flask &
 uv run python main.py app fastapi
