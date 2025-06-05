1a081622a460b9389753d07668c8837a7290b9ee
Naatie <muhammadqarani@gmail.com>
2025-06-05 14:34:03 +0700

    Activate virtual environment instead of using `uv run`

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-uv run python -u main.py worker api-pollers &
+. .venv/bin/activate
+# uvicorn dummyapi:app --host 0.0.0.0 --port 8000 &
+# python -u main.py worker api-pollers &
 
 # HTTP API. Flask is deprecated. Please use FastAPI
 # uv run python main.py app flask &
-uv run python main.py app fastapi
+python main.py app fastapi
