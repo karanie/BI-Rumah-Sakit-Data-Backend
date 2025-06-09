0b5322dd06a3c9d47dd796f5be1b6e990cfd2444
Naatie <muhammadqarani@gmail.com>
2025-06-09 21:58:33 +0700

    Add dependency that checks the API key header

--- app/fastapi/__init__.py
+++ app/fastapi/__init__.py
@@ -1,6 +1,27 @@
+import hmac
 import fastapi
+import config
 
-app = fastapi.FastAPI()
+api_key_header = fastapi.security.APIKeyHeader(name="X-Api-Key", auto_error=False)
+async def get_api_key(api_key: str = fastapi.Security(api_key_header)):
+    if not config.API_KEY:
+        return
+
+    if not api_key:
+        raise fastapi.HTTPException(
+            status_code=fastapi.status.HTTP_401_UNAUTHORIZED,
+            detail="Unauthorized"
+        )
+
+    if not hmac.compare_digest(api_key, config.API_KEY):
+        raise fastapi.HTTPException(
+            status_code=fastapi.status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid API Key."
+        )
+
+    return api_key
+
+app = fastapi.FastAPI(dependencies=[fastapi.Depends(get_api_key)])
 
 from .routers import test
 from .routers import demografi
