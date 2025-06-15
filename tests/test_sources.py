a825d93c3684f254d80890eb4cd118b2acdcc4eb
Naatie <muhammadqarani@gmail.com>
2025-06-15 22:33:54 +0700

    Slight improvement on `test_sources.py`

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,17 +1,17 @@
 import pytest
 import sources
+import polars as pl
+import pandas as pd
 
 def execute(backend, dataset_file):
     s = sources.file.SourceFile(backend=backend)
     df = s.read_dataset(dataset_file)
-    print(df)
     if backend == "pandas":
-        print(df.iloc[0])
+        assert type(df) == pd.DataFrame
     elif backend == "polars":
-        print(df.row(0))
+        assert type(df) == pl.DataFrame
 
 def test_sources_polars_file_csv(dataset_file_csv):
-    print(dataset_file_csv)
     execute("polars", dataset_file_csv)
 
 def test_sources_pandas_file_csv(dataset_file_csv):
@@ -20,9 +20,12 @@ def test_sources_pandas_file_csv(dataset_file_csv):
 def test_sources_api_object(dummyapi_server):
     import sources.api
     s = sources.api.SourceAPI(backend="object")
-    s.fetch("http://localhost:8089/api/data/dummy?datetime_start=2000-01-01+00%3A00%3A00")
+    res = s.fetch("http://localhost:8089/api/data/dummy?datetime_start=2000-01-01+00%3A00%3A00")
+    assert type(res) == list
 
 def test_sources_api_polars(dummyapi_server):
     import sources.api
+    import polars as pl
     s = sources.api.SourceAPI(backend="polars")
-    s.fetch("http://localhost:8089/api/data/dummy?datetime_start=2000-01-01+00%3A00%3A00")
+    res = s.fetch("http://localhost:8089/api/data/dummy?datetime_start=2000-01-01+00%3A00%3A00")
+    assert type(res) == pl.DataFrame
