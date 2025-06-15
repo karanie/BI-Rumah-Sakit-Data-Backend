1e6f9304efd33ebed18d0788e9b1b5d04e261656
Naatie <muhammadqarani@gmail.com>
2025-06-15 22:34:48 +0700

    Add `test_datastore.py` test set

---
+++ tests/test_datastore.py
@@ -0,0 +1,48 @@
+import pickle
+import gzip
+import pytest
+import polars as pl
+import pandas as pd
+import datastore.rdbms
+import datastore.parquet
+import datastore.file
+
+def test_datastore_parquet_polars(dataset_file_parquet):
+    dt = datastore.parquet.DatastoreParquet(backend="polars")
+    df = dt.read_parquet(dataset_file_parquet)
+    assert type(df) == pl.DataFrame
+
+def test_datastore_parquet_pandas(dataset_file_parquet):
+    dt = datastore.parquet.DatastoreParquet(backend="pandas")
+    df = dt.read_parquet(dataset_file_parquet)
+    assert type(df) == pd.DataFrame
+
+def test_datastore_file_pickle(dataset_file_pickle):
+    df = datastore.file.read_pickle(dataset_file_pickle)
+    assert type(df) == pd.DataFrame
+
+def test_datastore_file_pickle_gzip(dataset_file_pickle_gzip):
+    df = datastore.file.read_pickle(dataset_file_pickle_gzip)
+    assert type(df) == pd.DataFrame
+
+def test_datastore_file_save_as_pickle_compressed(tmp_path):
+    df = { "foo": "bar" }
+    filename = tmp_path / "foobar.pkl.gz"
+    datastore.file.save_dataset_as_pickle(df, str(filename))
+
+    with gzip.open(filename, "rb") as f:
+        read_obj = pickle.load(f)
+        assert type(read_obj) == dict
+        assert list(df.keys()) == ["foo"]
+        assert list(df.values()) == ["bar"]
+
+def test_datastore_file_save_as_pickle(tmp_path):
+    df = { "foo": "bar" }
+    filename = tmp_path / "foobar.pkl"
+    datastore.file.save_dataset_as_pickle(df, str(filename), compress=False)
+
+    with open(filename, "rb") as f:
+        read_obj = pickle.load(f)
+        assert type(read_obj) == dict
+        assert list(df.keys()) == ["foo"]
+        assert list(df.values()) == ["bar"]
